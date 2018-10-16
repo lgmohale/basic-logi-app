dfdda4a927d052022563f1745ae14c2e0c099eed
umuzi <lgmohale@outlook.com>
2018-10-16 16:47:15 +0200

    added hashing function

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+var flash = require('connect-flash');
 var serv = require('http').Server(app);
 var server;
 var db = require('./routes');
@@ -9,11 +12,19 @@ var bodyParser = require('body-parser')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.use(session({
+    secret:'work hard',
+    resave:true,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }
+}));
+app.use(flash());
+
 app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(req,res){
     res.sendFile("index.html");
-})
+});
 
 app.get('/user', function(req,res){
     res.sendFile("login.html");
@@ -21,3 +32,14 @@ app.get('/user', function(req,res){
 app.use('/user', db);
 var serv= app.listen(process.env.PORT || 4000);
  console.log("Server started.");
+
+ function create_local_memory (req,res,next){
+	
+    if(typeof(req.session.users) === 'undefined'){
+        
+        req.session.users ={};
+        console.log(`session created`);
+        
+        next()
+    };
+};
--- models/index.js
+++ models/index.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+let validator = require('validator');
+const bcrypt = require('bcrypt');
 mongoose.set('debug', true);
 mongoose.connect('mongodb://localhost/users');
 
@@ -18,6 +20,44 @@ var userSchema = new mongoose.Schema({
         type: String,
         require:true
     }
+});
+
+
+//autheticate a user before loging
+userSchema.statics.authenticate = function(email, password, callback){
+    users.findOne({email:email})
+    .exec(function(err,user){
+        if(err){
+            return callback(err)
+
+        }else if(!user){
+            var err = new Error('User not found');
+            err.status = 401;
+            return callback(err);
+
+        }
+        bcrypt.compare(password,user.password, function(err, result){
+            if(result == true){
+                return callback(null, user);
+
+            }else{
+                return callback();
+            }
+        })
+    })
+}
+
+//heashes password before it stores in the database
+userSchema.pre('save',function(next){
+    var user = this;
+    bcrypt.hash(user.password, 10, function(err, hash){
+        if(err){
+            return next(err)
+
+        }
+        user.password = hash;
+        next();
+    });
 });
 
 var users = mongoose.model('users', userSchema);
